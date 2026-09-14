eb40bacfd6f37fe283bf399a6e7a1cde24d7f01d
Viraj Karambelkar <karambelkarvraj21197@gmail.com>
2026-09-14 13:35:28 -0400

    pathify base_output_dir (#89)
    Co-authored-by: Robert David Stein <rdstein@caltech.edu>

--- snipergw/paths.py
+++ snipergw/paths.py
@@ -7,4 +7,7 @@ from pathlib import Path
 
 base_output_dir = os.getenv("SNIPERGW_DIR", Path.home().joinpath("Data/snipergw/"))
 
+if isinstance(base_output_dir, str):
+    base_output_dir = Path(base_output_dir)
+
 gwemopt_dir = Path(__file__).parent.joinpath("gwemopt")
